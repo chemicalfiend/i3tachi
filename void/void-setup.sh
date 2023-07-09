a3e91a974dba0147ada158ba5b6ab780d7a56db9
chemicalfiend <teamkagaku101@gmail.com>
2023-07-09 08:14:22 +0530

    updated the void side

---
+++ void/void-setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cp i3-config ~/.config/i3/config
+cp kitty.conf ~/.config/kitty/kitty.conf
+cp i3status.conf ~/.config/i3status/i3status.conf
+cp bashrc ~/.bashrc
+cp vimrc ~/.vimrc
+cp -R vim/colors ~/.vim/colors
+
+
